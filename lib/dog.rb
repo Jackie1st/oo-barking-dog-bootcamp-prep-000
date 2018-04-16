3a6a312eb89af6893860327eb9f7a31d407eddad
Jackie First <jackiejeep@gmail.com>
2018-04-16 14:05:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog 
-  def name=(new_name)
-    @name = new_name
-  end 
   def name 
     @name = name 
+  end 
+  def name=(new_name)
+    @name = new_name
   end 
   def bark 
     puts "Woof!"
